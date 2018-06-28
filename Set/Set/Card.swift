48ecf7f6d5238ea52333a5b846c148143bbf98e7
Karel Smejkal <karel.smejkal@campus.tu-berlin.de>
2018-06-28 20:22:16 +0200

    setting up env

---
+++ Set/Set/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Set
+//
+//  Created by Karel Smejkal on 6/28/18.
+//  Copyright © 2018 Karel Smejkal. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+}
